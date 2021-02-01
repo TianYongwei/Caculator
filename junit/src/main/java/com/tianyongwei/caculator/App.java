737144f2365d271a7946d8047f5c3bafbc797cd1
tianyongwei <869358189@qq.com>
2021-02-01 14:46:11 +0800

    first

---
+++ junit/src/main/java/com/tianyongwei/caculator/App.java
@@ -0,0 +1,13 @@
+package com.tianyongwei.caculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
